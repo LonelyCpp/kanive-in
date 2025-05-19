e8a59d4a863e9d5d4130e5e44b5a9b24397eee1e
Ananthu Kanive <ananthu.kanive@smallcase.com>
2025-05-19 19:36:50 +0530

    feat: add mix-blend-mode

--- src/routes/tools/image-matrix-css/+page.svelte
+++ src/routes/tools/image-matrix-css/+page.svelte
@@ -15,6 +15,8 @@
 	let invert = $state(0);
 	let opacity = $state(100);
 	let sepia = $state(0);
+	let backgroundColor = $state('#f9f9f9');
+	let mixBlendMode = $state('normal');
 </script>
 
 <svelte:head>
@@ -32,7 +34,7 @@
 <div
 	class="image-container"
 	style="--hue-rotate: {hueRotate}deg; --saturate: {saturate}%; --blur: {blur}px; --brightness: {brightness /
-		100}; --contrast: {contrast}%; --drop-shadow-x: {dropShadowX}px; --drop-shadow-y: {dropShadowY}px; --drop-shadow-blur: {dropShadowBlur}px; --grayscale: {grayscale}%; --invert: {invert}%; --opacity: {opacity}%; --sepia: {sepia}%"
+		100}; --contrast: {contrast}%; --drop-shadow-x: {dropShadowX}px; --drop-shadow-y: {dropShadowY}px; --drop-shadow-blur: {dropShadowBlur}px; --grayscale: {grayscale}%; --invert: {invert}%; --opacity: {opacity}%; --sepia: {sepia}%; --mix-blend-mode: {mixBlendMode}; --background-color: {backgroundColor}"
 >
 	<img src={img1} alt="test figure 1" />
 	<img src={img2} alt="test figure 2" />
@@ -41,34 +43,61 @@
 
 <div class="inputs">
 	<label
-		>Hue Rotate ({hueRotate}°)
+		><span class="label-text">Hue Rotate ({hueRotate}°)</span>
 		<input type="range" min={0} max={360} class="slider" id="hue-rotate" bind:value={hueRotate} />
 	</label>
 	<label>
-		Saturate ({saturate}%)
+		<span class="label-text">Saturate ({saturate}%)</span>
 		<input type="range" min={0} max={200} class="slider" id="saturate" bind:value={saturate} />
 	</label>
 
 	<label>
-		Brightness ({brightness}%)
+		<span class="label-text">Brightness ({brightness}%)</span>
 		<input type="range" min={0} max={200} class="slider" id="brightness" bind:value={brightness} />
 	</label>
 	<label>
-		Contrast ({contrast}%)
+		<span class="label-text">Contrast ({contrast}%)</span>
 		<input type="range" min={0} max={300} class="slider" id="contrast" bind:value={contrast} />
 	</label>
 
 	<label>
-		Invert ({invert}%)
+		<span class="label-text">Invert ({invert}%)</span>
 		<input type="range" min={0} max={100} class="slider" id="invert" bind:value={invert} />
 	</label>
 	<label>
-		Opacity ({opacity}%)
+		<span class="label-text">Opacity ({opacity}%)</span>
 		<input type="range" min={0} max={100} class="slider" id="opacity" bind:value={opacity} />
 	</label>
 	<label>
-		Sepia ({sepia}%)
+		<span class="label-text">Sepia ({sepia}%)</span>
 		<input type="range" min={0} max={100} class="slider" id="sepia" bind:value={sepia} />
+	</label>
+
+	<label>
+		<span class="label-text">BG Color ({backgroundColor})</span>
+		<input type="color" id="background-color" bind:value={backgroundColor} />
+	</label>
+
+	<label>
+		<span class="label-text">Mix Blend Mode</span>
+		<select id="mix-blend-mode" bind:value={mixBlendMode}>
+			<option value="normal">Normal</option>
+			<option value="hue">Hue</option>
+			<option value="multiply">Multiply</option>
+			<option value="screen">Screen</option>
+			<option value="overlay">Overlay</option>
+			<option value="darken">Darken</option>
+			<option value="lighten">Lighten</option>
+			<option value="color-dodge">Color Dodge</option>
+			<option value="color-burn">Color Burn</option>
+			<option value="hard-light">Hard Light</option>
+			<option value="soft-light">Soft Light</option>
+			<option value="difference">Difference</option>
+			<option value="exclusion">Exclusion</option>
+			<option value="saturation">Saturation</option>
+			<option value="color">Color</option>
+			<option value="luminosity">Luminosity</option>
+		</select>
 	</label>
 </div>
 
@@ -77,6 +106,8 @@
 		display: flex;
 		flex-direction: row;
 		gap: 10px;
+		padding: 50px;
+		background-color: var(--background-color);
 	}
 
 	img {
@@ -87,6 +118,7 @@
 			brightness(var(--brightness)) contrast(var(--contrast))
 			drop-shadow(var(--drop-shadow-x) var(--drop-shadow-y) var(--drop-shadow-blur) blue)
 			grayscale(var(--grayscale)) invert(var(--invert)) opacity(var(--opacity)) sepia(var(--sepia));
+		mix-blend-mode: var(--mix-blend-mode);
 	}
 
 	.inputs {
@@ -94,5 +126,11 @@
 		flex-direction: column;
 		gap: 10px;
 		margin-top: 24px;
+	}
+
+	.label-text {
+		display: inline-block;
+		font-size: 12px;
+		width: 150px;
 	}
 </style>
